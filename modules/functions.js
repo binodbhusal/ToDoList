36bb11b9460aa221db4ab64aee063cc19bd3b9e7
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-05-05 10:40:38 +0100

    complete add remove edit

--- modules/functions.js
+++ modules/functions.js
@@ -10,12 +10,17 @@ const taskPopulate = (tasks) => {
     <input type="checkbox" ${task.completed ? 'checked' : ''}id="${task.index}">
     <label for = "${task.index}"> "${task.description}"</label>
     <div class="remove"><button class="btnremove"><i class="fa">&#xf014;</i></button></div>
+    <div class="remove"><button class="btnEdit"><i class="fa-solid fa-pen-to-square"></i></button></div>
+
     </div> <hr>
     `).join('');
   const removeTask = (index) => {
+    for (let i = index + 1; i < tasks.length; i += 1) {
+      tasks[i].index -= 1;
+    }
     tasks.splice(index, 1);
-    setStorage(tasks);
     taskPopulate(tasks);
+    setStorage(tasks);
   };
 
   const btnRemove = document.querySelectorAll('.btnremove');
@@ -23,6 +28,48 @@ const taskPopulate = (tasks) => {
     btnRemove.forEach((btn, index) => {
       btn.addEventListener('click', () => {
         removeTask(index);
+      });
+    });
+  }
+  const editTask = (index) => {
+    const label = document.querySelector(`label[for='${index}']`);
+    const currentText = label.innerText.trim();
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.className = 'txtInput';
+    input.value = currentText;
+    label.replaceWith(input);
+    input.focus();
+    const saveChanges = () => {
+      const newText = input.value.trim();
+      if (newText !== '' && newText !== currentText) {
+        const newLabel = document.createElement('label');
+        newLabel.htmlFor = index;
+        newLabel.innerText = newText;
+        input.replaceWith(newLabel);
+        tasks[index].description = newText;
+        setStorage();
+      } else {
+        label.replaceWith(label);
+      }
+    };
+    input.addEventListener('blur', saveChanges);
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        saveChanges();
+      } else if (event.key === 'Escape') {
+        event.preventDefault();
+        label.replaceWith(label);
+      }
+    });
+  };
+
+  const btnEdit = document.querySelectorAll('.btnEdit');
+  if (btnEdit) {
+    btnEdit.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        editTask(index);
       });
     });
   }
